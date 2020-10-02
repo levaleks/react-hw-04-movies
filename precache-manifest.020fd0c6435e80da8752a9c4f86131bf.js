8543eead87f90ac64fee4443a350f0c54d78ccac
Oleksii Levchenko <>
2020-10-02 22:53:27 +0300

    Updates

---
+++ precache-manifest.020fd0c6435e80da8752a9c4f86131bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3365d17e91a1f9700448f0c0881d2a",
+    "url": "/react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "e52bbc818653d3a84249",
+    "url": "/react-hw-04-movies/static/css/main.2ecd79f3.chunk.css"
+  },
+  {
+    "revision": "2fb9d11d938ad8c6188f",
+    "url": "/react-hw-04-movies/static/js/0.68360537.chunk.js"
+  },
+  {
+    "revision": "06f1866c8663a3afc2d6",
+    "url": "/react-hw-04-movies/static/js/3.2298bfbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-04-movies/static/js/3.2298bfbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761f1de3cfd1adfd7d3b",
+    "url": "/react-hw-04-movies/static/js/4.bd6201e2.chunk.js"
+  },
+  {
+    "revision": "7b11e0bef913b6299013",
+    "url": "/react-hw-04-movies/static/js/5.e6761209.chunk.js"
+  },
+  {
+    "revision": "149a53643365aecb620f",
+    "url": "/react-hw-04-movies/static/js/6.f2884941.chunk.js"
+  },
+  {
+    "revision": "e52bbc818653d3a84249",
+    "url": "/react-hw-04-movies/static/js/main.9a28714c.chunk.js"
+  },
+  {
+    "revision": "ea647b915e1452e98312",
+    "url": "/react-hw-04-movies/static/js/runtime-main.22f76f9a.js"
+  }
+]);
